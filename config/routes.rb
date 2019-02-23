2ab35086123bc98889c74d0f3c8d83c73d1e7233
Christopher Beltran <christod.beltran@gmail.com>
2019-02-23 15:17:09 -0700

    Adding pets index page and controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/auth/failure', :to => 'sessions#failure'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  resources :pets, only: [:index]
+
 
 
   namespace :admin do
