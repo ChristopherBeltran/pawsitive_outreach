1e45089c314b6957fa0381b88c448ebd00592ad3
Christopher Beltran <christod.beltran@gmail.com>
2019-02-23 15:05:34 -0700

    adding helper methods in application controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_or_create_from_auth_hash(env["omniauth.auth"])
+    session[:user_id] = @user.id
+    redirect_to @user
   end
 
   def failure
+    redirect_to root_path
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
 end
