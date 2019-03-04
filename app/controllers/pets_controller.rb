121888ad4ab07e09d9b12b1586e52da5f4a9bd97
Christopher Beltran <christod.beltran@gmail.com>
2019-03-04 00:06:26 -0800

    fix for redirect logic for a logged in user on the users/:id/pets path

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,14 +2,14 @@ class PetsController < ApplicationController
   before_action :user_authenticate
 
   def index
-    if params[:user_id] && current_user.id == params[:user_id]
+    if params[:user_id] == nil
+      @pets = Pet.all
+    elsif params[:user_id] != nil && current_user == User.find_by(id: params[:user_id])
       @user_pets = current_user.pets
-    elsif
-      params[:user_id] && current_user.id != params[:user_id]
+    else
       redirect_to user_path(current_user)
     end
-  else
-    @pets = Pet.all
   end
-  
+
+
 end
