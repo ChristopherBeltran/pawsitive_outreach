89d495a8ab56c6f98290d64760830a3b013415ca
Christopher Beltran <christod.beltran@gmail.com>
2019-03-04 23:47:21 -0800

    Adding phone number format for user profile view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
 
-  def number_format
-    int_num = self.phone_number.to_i
+  def number_format(phone_number)
+    int_num = phone_number.to_i
     formatted_number = number_to_phone(int_num, area_code: true)
     return formatted_number
-  end 
+  end
+end
