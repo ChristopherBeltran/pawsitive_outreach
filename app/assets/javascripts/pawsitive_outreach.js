3c955ab9a11f8125a3977dfc09164de9b5f6c536
github username <github email address>
2019-05-21 20:53:06 -0700

    Fixing arguments for class constructors to intake an object

--- app/assets/javascripts/pawsitive_outreach.js
+++ app/assets/javascripts/pawsitive_outreach.js
@@ -5,29 +5,64 @@ class Breed {
         this.petId = petId;
         this.breedId = breedId;
     };
+
+      BreedCreator = {
+        createBreed: function () {
+          let newBreed = {};
+          Breed.apply(newBreed, arguments);
+          this.allBreeds.push(newBreed); 
+          return newBreed;
+        },
+      
+        allBreeds: [],
+      
+        forEachBreed: function (action) {
+          for (let i = 0; i < this.allBreeds.length; i++){
+            action.call(this.allBreeds[i]);
+          }
+        } 
+      };
 };
 
 class User {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
+    constructor(obj) {
+        this.id = obj.id;
+        this.name = obj.name;
     ;}
 };
 
 class Pet {
-    constructor(id, name, age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+    constructor(obj) {
+        this.id = obj.id;
+        this.name = obj.name;
+        this.age = obj.age;
     };
-}
 
+    PetCreator = {
+        createPet: function () {
+          let newPet = {};
+          Pet.apply(newPet, arguments);
+          this.allPets.push(newPet); 
+          return newPet;
+        },
+      
+        allPets: [],
+      
+        forEachPet: function (action) {
+          for (let i = 0; i < this.allPets.length; i++){
+            action.call(this.allPets[i]);
+          }
+        } 
+      };
+};
 
 //API calls
 
 function adminPetsIndex() {
     $.getJSON("/admin/pets.json", function(data) {
-        console.log(data);
+        let pets = data;
+        pets.map( p => 
+            PetCreator.createPet(p.id, p.name, p.age))
     });
 };
 
